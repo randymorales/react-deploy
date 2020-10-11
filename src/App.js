661bc4faa67d8aae28570ca89ee9306ee3f1b1b6
Randy <randymoralesg@gmail.com>
2020-10-10 21:16:06 -0600

    Deploy React app to Github Pages.
    Signed-off-by: Randy <randymoralesg@gmail.com>

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Test React app to test Github Pages deploy.
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
